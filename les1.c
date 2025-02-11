b571cbedca773e6a31d2f1eb9c319a72e874d70b
KotShaitan <kotshaitan17@gmail.com>
2025-02-11 09:54:14 +0300

    first task solved

--- les1.c
+++ les1.c
@@ -2,6 +2,12 @@
 
 int main(){
     int a;
+    printf("Enter your weight");
     scanf("%d", &a);
-    printf(a);
+    if (a > 60 && a < 90){
+        printf("You are Well!"); 
+    }
+    else {
+        printf("You are bad");
+    }
 }
